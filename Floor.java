e828416bf4684c088608980757ab9788b6e86636
rp22acg <raghu84yadav@gmail.com>
2024-08-01 22:17:49 +0100

    Floor code added

--- Floor.java
+++ Floor.java
@@ -1,4 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Floor
 {
+private int floorNo;
+private List<Slot> slots;
+
+
+public Floor(int floorNo, int numOfSlots)
+{
+    this.floorNo = floorNo;
+    slots=new ArrayList<>();
+    for(int i=1;i<=numOfSlots;i++)
+    {
+        if(i==1)
+        {
+            slots.add(new Slot(i, "Truck"));
+        }
+        else if(i == 2 || i == 3)
+        {
+            slots.add(new Slot(i, "Bike"));
+        }
+        else
+        {
+            slots.add(new Slot(i, "Cars"));
+        }
+    }
+
+}
+public List<Slot> getFreeSlots(String vehType)
+{
+    List<Slot> freeSlots=new ArrayList<>();
+    for(Slot slot:slots)
+    {
+        if(slot.getSlotType().equals(vehType) && slot.isFree())
+        {
+            freeSlots.add(slot);
+        }
+    }
+    return freeSlots;
+}
 
 }

ebb7eaa56f7c2109018e8a357d2039dfb94d0725
rp22acg <raghu84yadav@gmail.com>
2024-08-02 20:28:06 +0100

    ParkingLot code

--- Floor.java
+++ Floor.java
@@ -1,64 +1,56 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Floor
-{
-private int floorNo;
-private List<Slot> slots;
+public class Floor {
+    private int floorNo;
+    private List<Slot> slots;
 
 
-public Floor(int floorNo, int numOfSlots)
-{
-    this.floorNo = floorNo;
-    slots=new ArrayList<>();
-    for(int i=1;i<=numOfSlots;i++)
-    {
-        if(i==1)
-        {
-            slots.add(new Slot(i, "Truck"));
+    public Floor(int floorNo, int numOfSlots) {
+        this.floorNo = floorNo;
+        slots = new ArrayList<>();
+        for (int i = 1; i <= numOfSlots; i++) {
+            if (i == 1) {
+                slots.add(new Slot(i, "Truck"));
+            } else if (i == 2 || i == 3) {
+                slots.add(new Slot(i, "Bike"));
+            } else {
+                slots.add(new Slot(i, "Cars"));
+            }
         }
-        else if(i == 2 || i == 3)
-        {
-            slots.add(new Slot(i, "Bike"));
-        }
-        else
-        {
-            slots.add(new Slot(i, "Cars"));
-        }
+
     }
 
-}
-public List<Slot> getFreeSlots(String vehType)
-{
-    List<Slot> freeSlots=new ArrayList<>();
-    for(Slot slot:slots)
-    {
-        if(slot.getSlotType().equals(vehType) && slot.isFree())
-        {
-            freeSlots.add(slot);
+    public List<Slot> getFreeSlots(String vehType) {
+        List<Slot> freeSlots = new ArrayList<>();
+        for (Slot slot : slots) {
+            if (slot.getSlotType().equals(vehType) && slot.isFree()) {
+                freeSlots.add(slot);
+            }
         }
+        return freeSlots;
     }
-    return freeSlots;
-}
-public List<Slot> getOccupiedSlots(String vehType)
-{
-    List<Slot> occupiedSlots=new ArrayList<>();
-    for(Slot slots:slots)
-    {
-        if(slots.getSlotType().equals(vehType) && slots.getIsOccupied())
-        {
-            occupiedSlots.add(slots);
-        }
-    }
-    return occupiedSlots;
-}
 
-public int getFreeCount(String vehType)
-{
-    return getFreeSlots(vehType).size();
-}
-public int getFloorNo()
-{
-    return floorNo;
-}
+    public List<Slot> getOccupiedSlots(String vehType) {
+        List<Slot> occupiedSlots = new ArrayList<>();
+        for (Slot slots : slots) {
+            if (slots.getSlotType().equals(vehType) && slots.getIsOccupied()) {
+                occupiedSlots.add(slots);
+            }
+        }
+        return occupiedSlots;
+    }
+
+
+    public int getFreeCount(String vehType) {
+        return getFreeSlots(vehType).size();
+    }
+
+    public int getFloorNo() {
+        return floorNo;
+    }
+
+    public List<Slot> getSlots() {
+        return slots;
+    }
 }
--- ParkingLot.java
+++ ParkingLot.java
@@ -19,11 +19,65 @@ public String findAndParkVehicle(Vehicle vehicle)
         for(Slot slot:floor.getFreeSlots(vehicle.getVehType()))
         {
             if(slot.parkVehicle(vehicle))
-                return String.format(" Parked Vehicle, Ticke-ID: %s_%d_%d", id, floor.getFloorNo(), slot.getSlotNo());
+                return String.format(" Parked Vehicle, Ticket-ID: %s_%d_%d", id, floor.getFloorNo(), slot.getSlotNo());
         }
     }
     return "Parking is Full";
 }
+
+public String  unParkVehicle(String ticketId)
+{
+    String[] parts=ticketId.split("_");
+    if(parts.length!=3 && !parts[0].equals(id))
+    {
+        return "Invalid TicketID";
+    }
+    int floorNo=Integer.parseInt(parts[1]);
+    int slotNo=Integer.parseInt(parts[2]);
+    Floor floor=floors.get(floorNo-1);
+    Slot slot=floor.getSlots().get(slotNo-1);
+
+    Vehicle vehicle=slot.unParkVehicle();
+    if(vehicle!=null)
+    {
+        return String.format("Unpark vehicle with registration no: %s and color: %s ",vehicle.getRegNo(), vehicle.getVehColor());
+    }
+    return "Invalid Ticket";
+}
+ public void displayFreeCount(String vehType)
+ {
+     for(Floor floor:floors)
+     {
+         System.out.println("No of slots  for %s on floor %s: %s%n",vehType, floor.getFloorNo(), floor.getFreeCount(String vehType));
+     }
+ }
+
+ public void displayFreeSlots(String vehType)
+ {
+     for(Floor floor:floors)
+     {
+         List<Slot> Slots=floor.getFreeSlots(vehType);
+         System.out.println("Free slots for %s on the floor %s: %s%n",vehType, floor.getFloorNo(), formatSlotNumber(Slots));
+     }
+ }
+ public void displayOccupiedSlots(String vehType)
+ {
+     for(Floor floor:floors)
+     {
+         List<Slot> occupiedSlot=floor.getOccupiedSlots(vehType);
+         System.out.println("Occupied slots for %s on floor %s: %s%n",vehType, floor.getFloorNo(), formatSlotNumber(occupiedSlot));
+     }
+ }
+ public String formatSlotNumber(List<Slot> slot)
+ {
+     List<String> slotNumber=new ArrayList<>();
+     for(Slot slots:slot)
+     {
+         slotNumber.add(String.valueOf(slots.getSlotNo()));
+     }
+     return String.join(",", slotNumber);
+ }
+
 
 
 

0de7f5ac5695773bf9622d9c84b918247b9ad0c5
rp22acg <raghu84yadav@gmail.com>
2024-08-02 17:50:22 +0100

    ParkingLot code

--- ParkingLot.java
+++ ParkingLot.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLot {
+    private String id;
+    private List<Floor> floors;
+
+    public ParkingLot(String id, int numFloors, int slotsPerFloor) {
+        this.id = id;
+        this.floors = new ArrayList<>();
+        for (int i = 1; i <= numFloors; i++) {
+            floors.add(new Floor(i, slotsPerFloor));
+        }
+    }
+
 }
